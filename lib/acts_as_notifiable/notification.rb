1a3951038a6d70147870d8240ecf5fdfaaebbf16
Valentin Druon <valentin@dividom.com>
2016-10-03 17:35:35 +0200

    Added scopes for Notification

--- lib/acts_as_notifiable/notification.rb
+++ lib/acts_as_notifiable/notification.rb
@@ -18,5 +18,31 @@ module ActsAsNotifiable
 
     validates_presence_of :body
     validates_length_of :body, maximum: 255
+
+    ### SCOPES:
+
+    def self.from(notifier)
+      joins(:notifyings).
+      where(["notifyings.notifier_type = ? AND notifyings.notifier_id = ?", notifier.class.name, notifier.id]).
+      select("notifications.*")
+    end
+
+    def self.to(notified)
+      joins(:notifyings).
+      where(["notifyings.notified_type = ? AND notifyings.notified_id = ?", notified.class.name, notified.id]).
+      select("notifications.*")
+    end
+
+    def self.for(notifiable)
+      joins(:notifyings).
+      where(["notifyings.notifiable_type = ? AND notifyings.notifiable_id = ?", notifiable.class.name, notifiable.id]).
+      select("notifications.*")
+    end
+
+    def self.for_class(notifiable_class)
+      joins(:notifyings).
+      where(["notifyings.notifiable_type = ?", notifiable_class]).
+      select("notifications.*")
+    end
   end
 end
